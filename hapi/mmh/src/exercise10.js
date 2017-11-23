aeb29b7dab372515cffdb61e577ab7c9c6f83b7b
foxdonut <fdaoud@fastmail.com>
2017-11-23 11:51:12 -0500

    Hapi Joi validation exercise.

---
+++ hapi/mmh/src/exercise10.js
@@ -0,0 +1,41 @@
+/** global __dirname **/
+const Hapi = require("hapi");
+const Joi = require("joi");
+const path = require("path");
+
+const server = new Hapi.Server();
+
+server.connection({
+  host: "localhost",
+  port: Number(process.argv[2]) || 8400
+});
+
+server.route([
+  {
+    path: "/login",
+    method: "POST",
+    handler: function(request, reply) {
+      reply("login successful");
+    },
+    config: {
+      validate: {
+        payload: Joi.object({
+          isGuest: Joi.boolean().required(),
+          username: Joi.string().when("isGuest", { is: false, then: Joi.required() }),
+          accessToken: Joi.string().alphanum(),
+          password: Joi.string().alphanum()
+        })
+        .options({ allowUnknown: true })
+        .xor("accessToken", "password")
+      }
+    }
+  }
+]);
+
+server.start(function(err) {
+  if (err) {
+    throw err;
+  }
+  console.log("Server running on", server.info.uri);
+});
+
