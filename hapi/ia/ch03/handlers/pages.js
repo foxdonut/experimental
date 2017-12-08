02bf628509d9e565abee72a293939c3a28095f63
foxdonut <fdaoud@fastmail.com>
2017-12-08 17:42:31 -0500

    Copy example over

---
+++ hapi/ia/ch03/handlers/pages.js
@@ -0,0 +1,8 @@
+"use strict";
+
+exports.publicDir = {
+  directory: {
+    path: "public"
+  }
+};
+
---
+++ hapi/ia/ch03/routes.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const Pages = require("./handlers/pages");
+const Recipes = require("./handlers/recipes");
+
+module.exports =
+  [ { method: "GET"
+    , path: "/api/recipes"
+    , handler: Recipes.findAll
+    }
+  , { method: "GET"
+    , path: "/api/recipes/{id}"
+    , handler: Recipes.findOne
+    }
+  , { method: "POST"
+    , path: "/api/recipes"
+    , config: { auth: "api" }
+    , handler: Recipes.create
+    }
+  , { method: "POST"
+    , path: "/api/recipes/{id}/star"
+    , config: { auth: "api" }
+    , handler: Recipes.star
+    }
+  , { method: "GET"
+    , path: "/{filename*}"
+    , handler: Pages.publicDir
+    }
+  ];
+
