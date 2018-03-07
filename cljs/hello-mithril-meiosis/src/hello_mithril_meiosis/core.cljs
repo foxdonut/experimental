fc4380a900f2820fda0bb315ee1bd32857d4477f
foxdonut <fdaoud@fastmail.com>
2018-03-07 08:32:49 -0500

    Whew! Got cljs to work with shadow.

--- cljs/hello-mithril-meiosis/src/hello_mithril_meiosis/core.cljs
+++ cljs/hello-mithril-meiosis/src/hello_mithril_meiosis/core.cljs
@@ -1,19 +1,18 @@
 (ns hello-mithril-meiosis.core
-  ;(:require ["mithril" :as m] ["mithril-stream"]))
-  (:require ["mithril" :as m] ["flyd" :as flyd]))
+  (:require ["mithril" :as m] ["mithril-stream" :as stream]))
 
 (defn create-view [update-stream]
   (let [increase (fn [amount] (fn [event] (update-stream amount)))]
     (fn [model] (m "div"
-      (m "div" (str "Value: " model))
-      (m "button" { "onclick" (increase  1) } "+1")
-      (m "button" { "onclick" (increase -1) } "-1")
+      (m "div" (str "Counter: " model))
+      (m "button" (js-obj "onclick" (increase  1)) "+1")
+      (m "button" (js-obj "onclick" (increase -1)) "-1")
     ))))
 
-(def update-stream (.stream flyd))
+(def update-stream (stream))
 (def view (create-view update-stream))
 
-(def models (.scan flyd (fn [model value] (+ model value)) 0 update-stream))
+(def models (.scan stream (fn [model value] (+ model value)) 0 update-stream))
 
 (.map models (fn [model] (.render m js/app (view model))))
 
